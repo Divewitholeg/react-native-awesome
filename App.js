be5fbb5820d4c62379fa6212bdbbb2e81ed88eb3
vuewebdev <rybniko@gmail.com>
2023-05-12 18:06:28 +0300

    styles Stylesheet.create({});

--- App.js
+++ App.js
@@ -4,18 +4,49 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Hello from Oleg!</Text>
+      <Text style={[styles.titleOne, styles.pinkDiv]}>Hello from Oleg!</Text>
+      <View style={styles.pageElementsContainer}>
+        <Text style={styles.pageElementTextTitle}>One</Text>
+        <Text style={styles.pageElementTextTitle}>Two</Text>
+        <Text style={styles.pageElementTextTitle}>Three</Text>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  titleOne: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "teal",
+  },
+  pinkDiv: {
+    backgroundColor: "pink",
+    padding: 20,
+    borderRadius: 10,
+  },
   container: {
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  pageElementsContainer: {
+    flex: 1,
+
+    alignItems: "center",
+    justifyContent: "space-around",
+  },
+  pageElementTextTitle: {
+    backgroundColor: "fuchsia",
+    fontSize: 24,
+    minWidth: 250,
+    minHeight: 150,
+    fontWeight: "bold",
+    color: "white",
+    textAlign: "center",
+    borderRadius: 10,
   },
 });
